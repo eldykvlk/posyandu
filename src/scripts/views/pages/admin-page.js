6bc9601c0efe8b41c3c871ca0b7edde35eddeab2
eldykvlk <eldykoplak@gmail.com>
2024-02-11 13:07:57 +0700

    menghilangkan navbar

--- src/scripts/views/pages/admin-page.js
+++ src/scripts/views/pages/admin-page.js
@@ -1,39 +1,7 @@
 const Admin = {
 async render(){
 	return `
-   <nav class="navbar bg-body-primary fixed-top">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">POSYANDU EDELWEIS</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-      <div class="offcanvas-header">
-        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Menu Aplikasi</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-      </div>
-      <div class="offcanvas-body">
-        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#/">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Info Aplikasi</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Fitur Utama Admin
-            </a>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="pengumuman-page-admin.html">Catat Pengumuman</a></li>
-              <li><a class="dropdown-item" href="https://script.google.com/macros/s/AKfycbwvxkszbmmgUDABEVNdt--an3iuF-t7HFiCSNFJstUyTSslcoP8rdM8M0VKl4XNPnbA/exec">Catat Pelayanan</a></li>
-            </ul>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </div>
-</nav>
+<br><br>
 
 
     <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
