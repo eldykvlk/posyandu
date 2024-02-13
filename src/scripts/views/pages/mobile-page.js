37ccd88bd0363907d3f7e6556b72947c8d08d750
eldykvlk <eldykoplak@gmail.com>
2024-02-14 00:39:03 +0700

    memperbaiki jarak margin atas body

--- src/scripts/views/pages/mobile-page.js
+++ src/scripts/views/pages/mobile-page.js
@@ -1,8 +1,6 @@
 const Mobile = {
 async render(){
 	return `
-
-  
     <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
         <div class="carousel-inner">
             <div class="carousel-item active">
