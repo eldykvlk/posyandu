306411f88ade8bed085e062f4c50a4440d594e1b
eldykvlk <eldykoplak@gmail.com>
2024-07-29 13:55:09 +0700

    menghapus halaman laporan

--- webpack.common.js
+++ webpack.common.js
@@ -70,11 +70,6 @@ module.exports = {
       inject: false,
     }),
     new HtmlWebpackPlugin({
-      filename: 'laporan-admin.html',
-      template: path.resolve(__dirname, 'src/scripts/views/pages/laporan-admin.html'),
-      inject: false,
-    }),
-    new HtmlWebpackPlugin({
       filename: 'user-chartkms.html',
       template: path.resolve(__dirname, 'src/scripts/views/pages/user-chartkms.html'),
       inject: false,
